49dbeef8b88cea74ef04b29b1cf334f3d2e147af
Nathan Harris <harrihaven2@gmail.com>
2025-08-29 16:34:36 -0400

    initial commit

---
+++ lua/mappings.lua
@@ -0,0 +1,12 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map("t", "<ESC>", "<C-\\><C-n>")
+
+vim.keymap.del('n', '<S-Tab>')
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
